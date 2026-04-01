10fb51d5878cec0ca93b0718e3f96ae42789ea7a
Andres Felipe Garcia Camacho <est.andresf.garcia@unimilitar.edu.co>
2026-04-01 12:32:48 -0500

    correccion de metodos reales de listaEnlazada para nuestra clase PILAPREMIO

--- src/main/java/com/mycompany/discoteca_project/PILAPREMIO.java
+++ src/main/java/com/mycompany/discoteca_project/PILAPREMIO.java
@@ -2,36 +2,39 @@ package com.mycompany.discoteca_project;
 
 import com.murcia.utils.ListaEnlazada;
 
+import com.murcia.utils.ListaEnlazada;
+
 public class PILAPREMIO {
-    private ListaEnlazada premios;
+    private ListaEnlazada<String> premios;
 
     public PILAPREMIO() {
-        premios = new ListaEnlazada();
+        premios = new ListaEnlazada<>();
     }
 
-    // Apilar premio 
+    // Apilar premio
     public void apilar(String premio) {
-        premios.insertarInicio(premio); 
+        premios.addFirst(premio); // método correcto
     }
 
-    // Desapilar premio 
+    // Desapilar premio
     public String desapilar() {
-        if (premios.estaVacia()) {
+        if (premios.size() == 0) {
             return null;
         }
-        String premio = (String) premios.obtenerInicio();
-        premios.eliminarInicio();
+        String premio = premios.getFirst(); 
+        premios.remove(); 
         return premio;
     }
 
     // Mostrar todos los premios
     public void mostrarPremios() {
         System.out.println("Premios en la pila:");
-        premios.mostrar();
+        System.out.println(premios.toString()); 
     }
 
     // Verificar si la pila está vacía
     public boolean estaVacia() {
-        return premios.estaVacia();
+        return premios.size() == 0;
     }
 }
+
