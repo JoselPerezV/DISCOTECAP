33013142038ed1edd998df904b7d655180aca7c7
Andres Felipe Garcia Camacho <est.andresf.garcia@unimilitar.edu.co>
2026-04-01 12:17:07 -0500

    Apilar y Desapilar en clase PILAPREMIO para dar premios en la discoteca

--- src/main/java/com/mycompany/discoteca_project/PILAPREMIO.java
+++ src/main/java/com/mycompany/discoteca_project/PILAPREMIO.java
@@ -1,31 +1,37 @@
 package com.mycompany.discoteca_project;
 
-import com.murcia.utils.ListaEnlazada; 
+import com.murcia.utils.ListaEnlazada;
 
 public class PILAPREMIO {
-    private ListaEnlazada premios; 
-    
-    public PILAPREMIO() {
-        private ListaEnlazada premios;
-        
-        public PILAPREMIO(){
-            premios = new ListaEnlazada();
-        }
-    }
-    
+    private ListaEnlazada premios;
 
-      public PASAPORTE desapilar() {
-       
-        return null;
-       
+    public PILAPREMIO() {
+        premios = new ListaEnlazada();
     }
-       public boolean estaVacia() { 
-       
-        return false;
-       
-       }
-       public void mostrarPremios() {
-       
+
+    // Apilar premio 
+    public void apilar(String premio) {
+        premios.insertarInicio(premio); 
+    }
+
+    // Desapilar premio 
+    public String desapilar() {
+        if (premios.estaVacia()) {
+            return null;
         }
-      
+        String premio = (String) premios.obtenerInicio();
+        premios.eliminarInicio();
+        return premio;
+    }
+
+    // Mostrar todos los premios
+    public void mostrarPremios() {
+        System.out.println("Premios en la pila:");
+        premios.mostrar();
+    }
+
+    // Verificar si la pila está vacía
+    public boolean estaVacia() {
+        return premios.estaVacia();
+    }
 }
