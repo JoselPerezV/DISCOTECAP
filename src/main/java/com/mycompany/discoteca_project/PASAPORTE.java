37eea33b350ab6f43547fea77aa5b38dac2b397d
Jose Luis Perez Vera <est.josel.perez@unimilitar.edu.co>
2026-04-09 10:02:37 -0500

    Clase PASAPORTE Constructor de la clase modificado

--- src/main/java/com/mycompany/discoteca_project/PASAPORTE.java
+++ src/main/java/com/mycompany/discoteca_project/PASAPORTE.java
@@ -3,10 +3,21 @@ package com.mycompany.discoteca_project;
 
 public class PASAPORTE {
     
-    //atributos de la clase
+ //atributos de la clase
   private String numero;
   private String nombre;
   private String nacionalidad;
   private String fechaExpedicion;
   private String fechaVencimiento;
+  
+  //constructor MODIFICADo
+  public PASAPORTE(String numero, String nombre, String nacionalidad,String fechaExpedicion,String fechaVencimiento ){
+  this.numero = numero;
+  this.nombre = nombre;
+  this.nacionalidad=nacionalidad;
+  this.fechaExpedicion= fechaExpedicion;
+  this.fechaVencimiento= fechaVencimiento;
+  }
+  
+  
 }
