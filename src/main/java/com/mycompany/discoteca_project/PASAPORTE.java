b420b75e8af00ab3501cd5265061be1882239122
Jose Luis Perez Vera <est.josel.perez@unimilitar.edu.co>
2026-04-12 20:24:52 -0500

    Clase PASAPORTE Modificada  por Jose otra vez.

--- src/main/java/com/mycompany/discoteca_project/PASAPORTE.java
+++ src/main/java/com/mycompany/discoteca_project/PASAPORTE.java
@@ -10,14 +10,46 @@ public class PASAPORTE {
   private String fechaExpedicion;
   private String fechaVencimiento;
   
-  //constructor MODIFICADo
-  public PASAPORTE(String numero, String nombre, String nacionalidad,String fechaExpedicion,String fechaVencimiento ){
-  this.numero = numero;
-  this.nombre = nombre;
-  this.nacionalidad=nacionalidad;
-  this.fechaExpedicion= fechaExpedicion;
-  this.fechaVencimiento= fechaVencimiento;
-  }
+  //constructor
+    public PASAPORTE(String numero, String nombre, String nacionalidad,
+                     String fechaExpedicion, String fechaVencimiento){
+        this.numero = numero;
+        this.nombre = nombre;
+        this.nacionalidad = nacionalidad;
+        this.fechaExpedicion = fechaExpedicion;
+        this.fechaVencimiento = fechaVencimiento;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public String getFechaExpedicion() {
+        return fechaExpedicion;
+    }
+
+    public String getFechaVencimiento() {
+        return fechaVencimiento;
+    }
   
-  
+    //mostrar informacion del pasaporte
+    public void mostrarInfo(){
+        System.out.println("Pasaporte #: "+ numero);
+        System.out.println("Titular: "+nombre);
+        System.out.println("Nacionalidad: "+ nacionalidad);
+        System.out.println("Expedido: "+fechaExpedicion);
+        System.out.println("Vence: "+fechaVencimiento);
+    }
+  //verificar si esta vigente   
+public boolean estaVigente(String fechaActual){
+return fechaActual.compareTo(fechaVencimiento) <= 0; 
+}
 }
