20335d4e0ec132ebd59f6baac238daca018dac26
Jose Luis Perez Vera <est.josel.perez@unimilitar.edu.co>
2026-04-12 20:31:58 -0500

    Clase PILAPREMIO Constructor.

---
+++ src/main/java/com/mycompany/discoteca_project/PILAPREMIO.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.discoteca_project;
+import com.murcia.utils.ListaEnlazada;
+
+public class PILAPREMIO {
+    private ListaEnlazada<String> premios;
+    //constructor
+    public PILAPREMIO(){
+    premios = new ListaEnlazada<>() {};
+    }
+}
