4f077439d5674d4824eae07b9437cf0ca221b26b
Jose Luis Perez Vera <est.josel.perez@unimilitar.edu.co>
2026-04-13 17:13:03 -0500

    Clase PILAPREMIO MEtodos Apilar y desapilar premio.

--- src/main/java/com/mycompany/discoteca_project/PILAPREMIO.java
+++ src/main/java/com/mycompany/discoteca_project/PILAPREMIO.java
@@ -8,4 +8,21 @@ public class PILAPREMIO {
   premios = new ListaEnlazada<>() {};
   }
 
+  //apilar premio
+  public void apilar (String premio){
+  premios.addFirst(premio);
+  }
+  
+  //Desapilar premio
+  public String desapilar(){
+  if(premios.size()==0){
+      System.out.println("No hay premios en la pila");
+      return null;
+  }
+  String premio = premios.getFirst();
+  premios.remove();
+  return premio;
+  }
+  
+  
 }
