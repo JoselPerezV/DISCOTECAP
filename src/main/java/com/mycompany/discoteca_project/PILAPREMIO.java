a1161bfd978623114971b04f256eb762db844c65
Jose Luis Perez Vera <est.josel.perez@unimilitar.edu.co>
2026-04-13 16:59:25 -0500

    Clase PILAPREMIO Atributos y constructor de la clase.

--- src/main/java/com/mycompany/discoteca_project/PILAPREMIO.java
+++ src/main/java/com/mycompany/discoteca_project/PILAPREMIO.java
@@ -3,40 +3,9 @@ package com.mycompany.discoteca_project;
 import com.murcia.utils.ListaEnlazada;
 
 public class PILAPREMIO {
-    private ListaEnlazada<String> premios;
-    //constructor
-    public PILAPREMIO(){
-    premios = new ListaEnlazada<>() {};
-    }
-    
-    
-    //apilar premio
-public void apilar (String premio){
-premios.addFirst(premio);
-}
-
-//Desapilar premio
-public String desapilar(){
-if (premios.size() == 0){
-    System.out.println("No Hay Premios");
-    return null;
-}
-String premio = premios.getFirst();
-premios.remove();
-return premio;
-}
-//Mostrar todos los premios
-public void mostrarPremios(){
-if(premios.size() == 0){
-    System.out.println("No hay premios");
-    return;
-}
-    System.out.println("Premios en la pila: ");
-    System.out.println(premios.toString());
-}
-//verificar que la pila esta vacia
-public boolean estaVacia(){
-return premios.size()== 0;
-}
+  private ListaEnlazada<String> premios;
+  public PILAPREMIO(){
+  premios = new ListaEnlazada<>() {};
+  }
 
 }
