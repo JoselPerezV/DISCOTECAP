d46e6630a69b2319442cf0f9906fe103a1834b24
Jose Luis Perez Vera <est.josel.perez@unimilitar.edu.co>
2026-04-14 11:16:28 -0500

    Clase DISCOTECAEVENT Modificada solucionda logica y errores.

--- src/main/java/com/mycompany/discoteca_project/DISCOTECAEVENT.java
+++ src/main/java/com/mycompany/discoteca_project/DISCOTECAEVENT.java
@@ -2,37 +2,37 @@
 package com.mycompany.discoteca_project;
 
 public class DISCOTECAEVENT {
-    private String nombreEvento;
+    private String nombre;
     private String fecha;
-    private int bonusVisitas;         
-    private String premioEspecial;     
+    private int bonusVisitas;
+    private String premioEspecial;
 
     // Constructor
-    public DISCOTECAEVENT(String nombreEvento, String fecha, int bonusVisitas, String premioEspecial) {
-        this.nombreEvento = nombreEvento;
+    public DISCOTECAEVENT(String nombre, String fecha, int bonusVisitas, String premioEspecial) {
+        this.nombre = nombre;
         this.fecha = fecha;
-        this.premioEspecial = premioEspecial;
         this.bonusVisitas = bonusVisitas;
+        this.premioEspecial = premioEspecial;
     }
 
-    // Aplica el bonus de visitas al cliente
+    // Getters
+    public String getNombre() { return nombre; }
+    public String getFecha() { return fecha; }
+    public int getBonusVisitas() { return bonusVisitas; }
+    public String getPremioEspecial() { return premioEspecial; }
+
+    // Aplicar bonus de visitas a un cliente
     public void aplicarBonus(CLIENTE cliente) {
-        int visitasActuales = cliente.getVisitas();
-        cliente.setVisitas(visitasActuales + bonusVisitas);
-        System.out.println("Se aplicó bonus de " + bonusVisitas + " visitas a " + cliente.getNombre());
+        cliente.sumarVisitas(bonusVisitas);
+        System.out.println("Evento '" + nombre + "' aplicado: +" + bonusVisitas + " visitas a " + cliente.getNombre());
+        cliente.verificarRecompensas();
     }
 
-    // Agrega el premio especial a la pila de premios
-    public void agregarPremioEspecial(PILAPREMIO pila) {
-        pila.apilar(premioEspecial);
-        System.out.println("Se agregó el premio especial '" + premioEspecial + "' a la pila.");
-    }
-
-    // Muestra la información del evento
-    public void mostrarEvento() {
-        System.out.println(" Evento: " + nombreEvento);
-        System.out.println(" Fecha: " + fecha);
-        System.out.println(" Premio especial: " + premioEspecial);
-        System.out.println(" Bonus de visitas: " + bonusVisitas);
+    // Mostrar información del evento
+ public void mostrarEvento() {
+        
+    System.out.println("Evento: " + nombre + " | Fecha: " + fecha +
+                           " | Bonus: " + bonusVisitas +
+                           " | Premio especial: " + premioEspecial);
     }
 }
